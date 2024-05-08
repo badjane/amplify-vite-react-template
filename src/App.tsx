f825617e2450a26943043f5a223e8496bdd04bc2
= <1842752325@qq.com>
2024-05-08 17:47:01 +0800

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -17,13 +21,23 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
-  return (
+
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
+    return (
+
+        <Authenticator>
+            {({ signOut, user }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+            <li
+                onClick={() => deleteTodo(todo.id)}
+                key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -32,8 +46,12 @@ function App() {
         <a href="https://next-release-dev.d1ywzrxfkb9wgg.amplifyapp.com/react/start/quickstart/vite-react-app/#step-2-add-delete-to-do-functionality">
           Review next step of this tutorial.
         </a>
-      </div>
-    </main>
+                    </div>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+
+            )}
+        </Authenticator>
   );
 }
 
